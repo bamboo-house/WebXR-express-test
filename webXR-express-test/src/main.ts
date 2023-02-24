7922cf4d850720bf6e648e9f8cfe2ccb306d9752
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-02-25 00:15:17 +0900

    ngrokでhttpsやったらいけた

--- webXR-express-test/src/main.ts
+++ webXR-express-test/src/main.ts
@@ -6,7 +6,7 @@ const app = express();
 
 // パスに注意する。src/sample-scenesに置く場合は、staticの引数にsrc/sample-scenesを指定する。
 // npm startをする場所は、webXR-express-testディレクトリですること。
-app.use(express.static("sample-scenes6"));
+app.use(express.static("sample-scenes"));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
